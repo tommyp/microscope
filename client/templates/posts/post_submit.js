06fe5df12caff53c19cbe12b7c43a2075a4461e2
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-06-26 09:43:20 +0100

    User can now submit posts

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,16 @@
+Template.postSubmit.events({
+
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var post = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    };
+
+    console.log(e);
+
+    post._id = Posts.insert(post);
+    Router.go('postPage', post);
+  }
+});
